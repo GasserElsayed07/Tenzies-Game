22e88765374bd7aea9df9bbcbe7ce303d5e88957
Manga_Bl_Manga <gogo05tv@gmail.com>
2025-04-25 23:00:45 +0300

    Add: reroll button - the ability to hold die

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
 import Die from './components/Die.jsx'
-import {useState} from "react"
-export default function App(){
+import { useState } from "react"
 
-  function getRandNum(){
-    const arrOfNums = Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
+export default function App() {
+
+  function getArrObj() {
+    const arrOfNums = Array.from({ length: 10 }, () => ({ value: Math.ceil(Math.random() * 6), isHeld: false }));
     return arrOfNums;
   }
 
-  const [numbers, setNumbers] = useState(getRandNum())
+  // state of an array of objects that holds 2 values: (value, isHeld)
+  const [numbers, setNumbers] = useState(getArrObj())
 
-  console.log(numbers)
+  function reRoll() {
+    setNumbers(numbers.map((entry) => (
+      entry.isHeld == true ? entry : {...entry, value : Math.ceil(Math.random() * 6)}
+    )))
+  }
 
-  const buttons = numbers.map((entry,index) => {
+  const buttons = numbers.map((obj, index) => {
     return (
-      <Die value = {entry}
-        key = {index}
+      <Die 
+        obj={obj}
+        key={index}
+        id ={index}
+        hold = {hold}
       />
     )
   })
 
-  return(
+  function hold(index){
+    console.log("my id is " + index + " " + numbers.length)
+
+    setNumbers(prev => numbers.map((value,ind) => (
+      (index == ind) ? {...value, isHeld : !value.isHeld} : value
+    )))
+
+  }
+
+  return (
     <main>
       <section>
         {buttons}
       </section>
+
+      <button className='roll' onClick={reRoll}>ROLL</button>
     </main>
   )
 }
+
+/*
+  Create a button
+  onClick of this button => setNumbers(getRandNums)
+
+*/
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,16 @@
 import "../styles/Die.css"
-
 export default function Die(props){
 
+    const dieStyle = {
+        backgroundColor: props.obj.isHeld ? "lightGreen" : "white"
+    }
+
     return(
-        <button>{props.value}</button>
+        <button
+            style={dieStyle}
+            onClick={() => props.hold(props.id)}
+        >
+            {props.obj.value}
+        </button>
     )
 }
