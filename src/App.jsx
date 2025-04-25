82ce25cd3b808fbbaec515c974cb5a0aae2a7b29
Manga_Bl_Manga <gogo05tv@gmail.com>
2025-04-25 19:26:37 +0300

    Add random numbers to dies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import Die from './components/Die.jsx'
-
+import {useState} from "react"
 export default function App(){
+
+  function getRandNum(){
+    const arrOfNums = Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
+    return arrOfNums;
+  }
+
+  const [numbers, setNumbers] = useState(getRandNum())
+
+  console.log(numbers)
+
+  const buttons = numbers.map((entry,index) => {
+    return (
+      <Die value = {entry}
+        key = {index}
+      />
+    )
+  })
 
   return(
     <main>
       <section>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
+        {buttons}
       </section>
     </main>
   )
